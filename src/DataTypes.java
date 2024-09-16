2993812c8b290c2b8f44633074d7275fd133e61a
beam <108953955+bem-boi@users.noreply.github.com>
2024-09-16 18:07:32 -0400

    fix

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,10 +3,10 @@ import java.util.List;
 public class DataTypes {
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
-        for (int i = 0; i < numbers.size(); i++) {
-            s += numbers.get(i);
+        for (int x: numbers) {
+            s += x;
         }
         return s;
     }
