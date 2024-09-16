63ab5a1d648a79aee87f5e1fe18d92e3fc8d782a
beam <108953955+bem-boi@users.noreply.github.com>
2024-09-16 17:56:32 -0400

    on diff branch

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -6,7 +6,7 @@ public class DataTypes {
 
         int s = 0;
         // below is a "foreach" loop which iterates through numbers
-        for (int i = 0; i < numbers.size(); i++) {
+        for (int i = 0; i < numbers.size(); i++){
             s += numbers.get(i);
         }
         return s;
